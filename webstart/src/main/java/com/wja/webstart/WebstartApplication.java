d18c457e2a1ce07fe41653e0e10f85b538b58bb4
w-jadczak <w.jadczak@yahoo.com>
2023-09-10 13:23:16 +0200

    initial commit

---
+++ webstart/src/main/java/com/wja/webstart/WebstartApplication.java
@@ -0,0 +1,13 @@
+package com.wja.webstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstartApplication.class, args);
+	}
+
+}
---
+++ webstart/src/test/java/com/wja/webstart/WebstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wja.webstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
